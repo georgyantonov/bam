28c484c652e4eeb254b5d0deaa11ca6fdeee5817
georgyantonov <100569135+georgyantonov@users.noreply.github.com>
2022-08-10 23:29:48 +0300

    добавил play

--- src/Components/Main/Content/Content.jsx
+++ src/Components/Main/Content/Content.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import play from'../../../Assets/Play.svg'
+import './style.scss'
 
 export default function Content() {
   return (
     <div className='content'>
         <div className="years">50</div>
         <div className="abbreviation">БАМ</div>
+        <div className="play">
+          <img src={play} alt="Смотреть видео" />
+          Смотреть видео
+        </div>
     </div>
   )
 }
