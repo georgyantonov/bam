99b56ecb14f0a71c43864ae546580d429713b544
georgyantonov <100569135+georgyantonov@users.noreply.github.com>
2022-08-10 23:27:40 +0300

    навбар

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Logo from './Logo/Logo'
+import Nav from './Nav/Nav'
 import './style.scss'
 
 export default function Header() {
   return (
     <header className='header'>
+        <div className="header_items">
         <Logo></Logo>
+        <Nav></Nav>
+        </div>
     </header>
   )
 }
