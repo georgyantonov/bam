d35bcd67afa0c9afce0d7835efaac08f295a1819
georgyantonov <georgy.antonov@dalee.ru>
2022-08-12 17:07:31 +0300

    второй экран

--- src/Components/Main/About/About.jsx
+++ src/Components/Main/About/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './style.scss'
+import  wedding from '../../../Assets/weding.png'
+import employees from '../../../Assets/employees.png'
 
 export default function About() {
   return (
@@ -7,6 +9,18 @@ export default function About() {
         <div className="about_header">
             <span>БАЙКАЛО-</span>
             <br/>АМУРСКАЯ <br/>МАГИСТРАЛЬ
+        </div>
+        <div className="description">
+        грандиозный замысел, проект, который <br/> когда-то стал реальным благодаря<br/> героическому труду обычных людей
+        </div>
+        <div className="bam_info">
+          <div className="wedding">
+            <img src={wedding} alt="Свадьба" />
+          </div>
+          <div className="employees">
+            <img src={employees} alt="Строители" />
+            <div className="bg"></div>
+          </div>
         </div>
     </div>
   )
