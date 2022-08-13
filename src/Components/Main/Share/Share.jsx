653778ad4cced04299461d262c6cba3202735e70
georgyantonov <100569135+georgyantonov@users.noreply.github.com>
2022-08-13 22:43:11 +0300

    компонент формы

---
+++ src/Components/Main/Share/Share.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.scss'
+
+export default function Share() {
+  return (
+    <div className='share'>
+        <div className="share_container"></div>
+    </div>
+  )
+}
