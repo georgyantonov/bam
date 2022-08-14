8c1a9f9ba9e241736013d4376c1d906976bd3e49
georgyantonov <100569135+georgyantonov@users.noreply.github.com>
2022-08-14 22:42:44 +0300

    форма

--- src/Components/Main/Share/Share.jsx
+++ src/Components/Main/Share/Share.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import './style.scss'
+import vk from '../../../Assets/vk.svg'
 
 export default function Share() {
   return (
     <div className='share'>
-        <div className="share_container"></div>
+        <div className="share_container">
+          
+            <h1>поделитесь историей</h1>
+            <button className='vk'>
+              <img src={vk} alt="ВКонтакте" />
+              <div className='auth'>Войдите через VK ID</div>
+            </button>
+          <form>
+            <div className="row">
+              <div className="col">
+            <label htmlFor="email">
+              Email*    
+            </label>
+            <input type="text" className="email" name='email' placeholder='Электронный адрес' required />
+            </div>
+            <div className="col">
+            <label htmlFor="name">Имя*</label>
+            <input type="text" className="name" name='name' placeholder='Ваше имя' required />
+            </div>
+            </div>
+          </form>
+        </div>
     </div>
   )
 }
