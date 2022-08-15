fa32f85ddacb75a35e1c7e4d0e1e98e5130d2e88
georgyantonov <georgy.antonov@dalee.ru>
2022-08-15 18:31:49 +0300

    форма

--- src/Components/Main/Share/Share.jsx
+++ src/Components/Main/Share/Share.jsx
@@ -15,15 +15,51 @@ export default function Share() {
           <form>
             <div className="row">
               <div className="col">
-            <label htmlFor="email">
-              Email*    
+                <label htmlFor="email">
+                  Email*    
+                </label>
+                <input type="text" className="input email" name='email' placeholder='Электронный адрес' required />
+              </div>
+              <div className="col">
+                <label htmlFor="name">
+                  Имя*
+                </label>
+                <input type="text" className="input name" name='name' placeholder='Ваше имя' required />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <label htmlFor="hero">
+                  О ком ваша история*
+                </label>
+                <input type="text" className='input hero' name='hero' placeholder='Имя героя' required />
+              </div>
+            </div>
+            <label htmlFor="story">
+              Ваша история*
             </label>
-            <input type="text" className="email" name='email' placeholder='Электронный адрес' required />
-            </div>
-            <div className="col">
-            <label htmlFor="name">Имя*</label>
-            <input type="text" className="name" name='name' placeholder='Ваше имя' required />
-            </div>
+            <textarea name="story"  className='input story' placeholder='Расскажите о себе или о своих родственниках, которые принимали участие в строительстве магистрали'></textarea>
+            <div className="row">
+              <div className="col">
+              <label htmlFor="place">
+                    Место работы на БАМе
+                  </label>
+                <span className="icon">
+                  
+                  <input type="text" name='place' className="with_icon input place " placeholder='Место работы' />
+                </span>
+              </div>
+              <div className="col">
+              <div className="row">
+                <div className="col">
+                  <label htmlFor="from">
+                    Годы работы
+                  </label>
+                  <input type="date" className="with_icon from input" name='from' min='1972-04-05' max='1984-10-27' placeholder="05 апреля 1972 г."/>
+                </div>
+                <div className="col"></div>
+              </div>
+              </div>
             </div>
           </form>
         </div>
