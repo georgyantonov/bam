76f9b7aa1bf785fba417dad4ec7e2a1223d59825
georgyantonov <georgy.antonov@dalee.ru>
2022-08-30 09:46:05 +0300

    адаптивы

--- src/Components/Header/Nav/MobileNav.jsx
+++ src/Components/Header/Nav/MobileNav.jsx
@@ -1,5 +1,5 @@
 import './style.scss'
-import React from 'react'
+import React, { StrictMode } from 'react'
 import NavItems from './NavItems'
 import {AiOutlineClose} from 'react-icons/ai'
 import {AiOutlineMenu} from 'react-icons/ai'
@@ -8,17 +8,22 @@ import {CSSTransition} from 'react-transition-group'
 
 export default function MobileNav() {
   const [open,setOpen] = useState(false)
-  const close = <AiOutlineClose className="burger" onClick = { ()=> setOpen(!open)}/>;
-  const menu = <AiOutlineMenu className="burger" onClick = { (e)=> setOpen(!open)}/>;
+  const close = <AiOutlineClose className="burger" onClick = { (e)=> setOpen(!open)}/>;
+  const menu = <AiOutlineMenu className="burger" onClick = { (e)=> setOpen(!open) && scrollBlock}/>;
   const linkClick = () => setOpen(false);
+  function scrollBlock(e){
+    
+    open ? document.querySelector('body').style.overflow = '' : document.querySelector('body').style.overflow = 'hidden'
+  }
 
   return (
-    <div className='mobile_nav' >
+    <div className='mobile_nav' onClick={(e) => scrollBlock(e)}>
       {open ? close : menu}
       <CSSTransition
         in={open}
         onEnter= {() => setOpen(true)}
-        onClick = {() => setOpen(false)} >
+        onClick = {() => setOpen(false)} 
+        >
           <NavItems 
             isMobile = {true}
             linkClick = {linkClick}
